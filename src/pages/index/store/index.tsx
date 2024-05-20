9934531c95ed7d61796bddb2cf60926b00801db8
NguyenDat0809 <datngx.dev@gmail.com>
2024-05-20 17:40:14 +0700

    feat: thêm chức năng reset cart khi vào cửa hàng mới

--- src/pages/index/store/index.tsx
+++ src/pages/index/store/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC, Suspense } from "react";
-import { useRecoilValue, useResetRecoilState } from "recoil";
+import React, { FC, Suspense, useEffect } from "react";
+import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
 import { cartState, selectedStoreIdState, selectedStoreNameState } from "state";
 import { Box, Header, Page, useNavigate } from "zmp-ui";
 import { ProductList } from "../product-list";
@@ -11,8 +11,27 @@ import { CartIcon } from "components/cart-icon";
 import FloatingActionButton from "pages/FloatingActionButton";
 import { Divider } from "components/divider";
 import cart from "pages/cart";
+import { prepareCart } from "utils/product";
 
 const StorePage: FC = () => {
+  const [cart, setCart] = useRecoilState(cartState);
+  const currentPickedStore = useRecoilValue(selectedStoreIdState);
+  const ResetCart = () => {
+    setCart((prevCart) => {
+      let res = { ...prevCart };
+      res = {
+        ...prevCart,
+        productList: [],
+        totalQuantity: 0
+      };
+      return prepareCart(res);
+    });
+  };
+  useEffect(() => {
+    if (cart.storeId !== currentPickedStore) {
+      ResetCart();
+    }
+  }, []);
   const navigate = useNavigate();
   const handleFabClick = () => {
     navigate("/cart");
@@ -20,6 +39,7 @@ const StorePage: FC = () => {
     // Define the action for FAB click, e.g., navigate to a specific route
   };
   // const selectedStoreName = useRecoilValue(selectedStoreNameState);
+ 
  
   return (
     <Page className="flex flex-col ">
