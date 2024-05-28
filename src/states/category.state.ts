9112677a03e8b4d3473a049c11e845f32d552187
neru <ngocngocthuc@gmail.com>
2024-05-28 19:45:35 +0700

    refactor: category.state.ts

---
+++ src/states/category.state.ts
@@ -0,0 +1,48 @@
+import { atom, selector } from "recoil";
+import { menuByStoreState } from "./menu.state";
+import { Category, CategoryType } from "types/store-menu";
+
+export const currentCateState = selector({
+  key: "category",
+  get: async ({ get }) => {
+    const menu = get(menuByStoreState);
+    const cateId = get(selectedCategoryIdState);
+    const currentCate = menu.categories.find((cate) => cate.id === cateId);
+    if (currentCate !== undefined) {
+      return currentCate;
+    }
+    return null;
+  },
+});
+
+export const childCategoriesState = selector<Category[]>({
+  key: "childCategories",
+  get: async ({ get }) => {
+    const menu = get(menuByStoreState);
+    const cateId = get(selectedCategoryIdState);
+    const selectedCategory = menu.categories.find((cate) => cate.id === cateId);
+    const listChild = menu.categories.filter(
+      (cate) => cate.type === CategoryType.CHILD
+    );
+    console.log("select category id ", cateId);
+    console.log("select category ", selectedCategory);
+    console.log("list category child", listChild);
+    if (selectedCategory?.childCategoryIds === [] || listChild == null) {
+      return [];
+    }
+    const listChildofParentCate: Category[] = [];
+    listChild.forEach((e) => {
+      if (selectedCategory?.childCategoryIds.some((id) => id === e.id)) {
+        listChildofParentCate.push(e);
+      }
+    });
+    return listChildofParentCate.sort(
+      (a, b) => b.displayOrder - a.displayOrder
+    );
+  },
+});
+
+export const selectedCategoryIdState = atom({
+  key: "selectedCategoryId",
+  default: "",
+});
