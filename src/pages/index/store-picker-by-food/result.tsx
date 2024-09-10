d2a2fc551432ae30a9025177922670c0dee0fd94
NguyenDat0809 <datngx.dev@gmail.com>
2024-09-10 10:09:53 +0700

    modified: thay đổi CSS ở category page

--- src/pages/index/store-picker-by-food/result.tsx
+++ src/pages/index/store-picker-by-food/result.tsx
@@ -2,12 +2,13 @@ import React, { Suspense } from "react";
 import { FC } from "react";
 import { useNavigate } from "react-router";
 import { Box, Button, Icon, Text } from "zmp-ui";
-import {
-  useRecoilValueLoadable,
-  useSetRecoilState,
-} from "recoil";
+import { useRecoilValueLoadable, useSetRecoilState } from "recoil";
 //TODO: design
-import { selectedStoreIdState, storeState, storesByFoodCategoryState } from "states/store.state";
+import {
+  selectedStoreIdState,
+  storeState,
+  storesByFoodCategoryState,
+} from "states/store.state";
 import storeSkeleton from "../../../static/skeletons/store-skeleton.png";
 import { Store } from "types/store";
 
@@ -20,59 +21,68 @@ export const StorePickedByFoodResult: FC = () => {
     setStoreSelected(store.id);
     navigate("/store");
   };
-  const storesByFoodCategory = useRecoilValueLoadable(storesByFoodCategoryState);
+  const storesByFoodCategory = useRecoilValueLoadable(
+    storesByFoodCategoryState
+  );
   switch (storesByFoodCategory.state) {
     case "loading" || "hasError":
       return (
         <Box className="flex-auto bg-background p-4 flex">
-          <Suspense fallback={<h1>Chưa có kết quả</h1>}>
-          </Suspense>
+          <Suspense fallback={<h1>Chưa có kết quả</h1>}></Suspense>
         </Box>
       );
     case "hasValue":
       const stores = storesByFoodCategory.contents;
+      // console.log(stores);
+      if (stores.length == 0 || stores == null)
+        return (
+          <Box className="flex-auto bg-background p-4 flex">
+            <Text>Không có kết quả phù hợp</Text>
+          </Box>
+        );
       return (
         <Box className="flex-auto bg-background p-4 flex">
           <Suspense fallback={<h1>Chưa có kết quả</h1>}>
             <div className="bg-white rounded-xl overflow-hidden p-0 w-full">
-              {stores.length == 0 ? <Text>Không có kết quả phù hợp</Text> : stores.map((store?) => store != null ? (
+              {stores.map((store) => (
                 <Box
-                  key={store.id}
+                  key={store!.id}
                   className="mb-5"
                   flex
-                  onClick={() => gotoStore(store)}
+                  onClick={() => gotoStore(store!)}
                 >
                   <div className="flex-none aspect-card relative w-3/12">
                     <img
                       src={storeSkeleton}
-                      className="absolute w-full h-full object-fit rounded-xl"
+                      className="absolute w-full h-full object-cover rounded-xl"
                     />
                   </div>
                   <Box my={4} mx={5} className="w-9/12">
-                    {/* {before} */}
-                    <Text.Title size="small">{store.name}</Text.Title>
-                    {/* {after} */}
+                    <Text.Title size="small">{store!.name}</Text.Title>
                     <Box mx={0} mb={0} flex>
                       <Button
                         prefixIcon={
-                          <Icon className="text-yellow-400" icon="zi-star-solid" />
+                          <Icon
+                            className="text-yellow-400"
+                            icon="zi-star-solid"
+                          />
                         }
                         size="small"
                         className="pl-0 relative"
                         variant="tertiary"
                       >
                         <span className="text-gray-500 font-semibold absolute top-2">
-                          {/* {restaurant.rating} */}5.0
+                          5.0
                         </span>
                       </Button>
                     </Box>
                   </Box>
                 </Box>
-              ) : null)}
+              ))}
             </div>
           </Suspense>
         </Box>
       );
   }
-  return <></>
+  return <></>;
 };
--- src/pages/index/store/detail.tsx
+++ src/pages/index/store/detail.tsx
@@ -1,34 +1,35 @@
 import React, { FC } from "react";
-import {  useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import { Box, Text } from "zmp-ui";
 import storeSkeleton from "../../../static/skeletons/store-skeleton.png";
 import { storeState } from "states/store.state";
 
 export const StoreDetail: FC = () => {
   const currentStoreState = useRecoilValue(storeState);
-    return (
-      <Box className="flex flex-col ">
-        <Box m={5}>
-          <div className="relative aspect-video w-full rounded-lg">
-            <img
-              src={storeSkeleton}
-              className="absolute w-full h-full object-cover "
-            />
-          </div>
-          <Box
-            mx={4}
-            className="relative bg-primary border rounded-lg border-primary text-center"
-            p={4}
-            style={{ marginTop: -60 }}
-          >
-            <Text className="font-bold">{currentStoreState.name || "Tên quán"}</Text>
-            <Text className="text-gray-500">{currentStoreState.address || "Địa chỉ"}</Text>
-          </Box>
+  return (
+    <Box className="flex flex-col ">
+      <Box m={5}>
+        {/* <div className="relative aspect-video rounded-lg"> */}
+        <div className="relative w-[320px] h-[200px] rounded-lg">
+          <img
+            src={storeSkeleton}
+            className="absolute w-full h-full object-cover "
+          />
+        </div>
+        <Box
+          mx={4}
+          className="relative bg-primary border rounded-lg border-primary text-center"
+          p={4}
+          style={{ marginTop: -60 }}
+        >
+          <Text className="font-bold">
+            {currentStoreState.name || "Tên quán"}
+          </Text>
+          <Text className="text-gray-500">
+            {currentStoreState.address || "Địa chỉ"}
+          </Text>
         </Box>
       </Box>
-    );
+    </Box>
+  );
 };
-
-
-
-
