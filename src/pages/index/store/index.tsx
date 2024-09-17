d3c3a0293cecf4bd10117feb9aeb957d8344a2e9
Magic Flexing <134113309+Nguyen-Huy-Phuc@users.noreply.github.com>
2024-09-17 15:02:34 +0700

    update: chỉnh sửa UI của shop

--- src/pages/index/store/index.tsx
+++ src/pages/index/store/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC, Suspense } from "react";
 import { useRecoilValueLoadable } from "recoil";
-import { Box, Header, Page, useNavigate, Text } from "zmp-ui";
-import { ProductList } from "../product-list";
+import { Box, Header, Page, useNavigate, Text, Icon } from "zmp-ui";
+import { Categories } from "../categories-list";
 import { StoreDetail } from "./detail";
 import { Collections } from "./collections";
 import { Divider } from "components/divider";
@@ -9,11 +9,14 @@ import { menuByStore } from "states/menu.state";
 import { ContentFallback } from "components/content-fallback";
 import { CartIcon } from "components/cart-icon/cart-icon";
 import FloatingActionButton from "components/cart-icon/FloatingActionButton";
+import { useRecoilValue } from "recoil";
+import { storeState } from "states/store.state";
 
 const StorePage: FC = () => {
   const navigate = useNavigate();
 
   const menu = useRecoilValueLoadable(menuByStore);
+  const currentStoreState = useRecoilValue(storeState);
   const handleFabClick = () => {
     navigate("/cart");
   };
@@ -22,15 +25,23 @@ const StorePage: FC = () => {
   }
   if (menu.state == "hasValue" && menu.contents !== null) {
     return (
-      <Page className="flex flex-col ">
+      <Page className="flex flex-col bg-white">
         <Box>
-          <Header showBackIcon={true} />
-          <StoreDetail />
-          <Divider />
+          <Header
+            showBackIcon={true}
+            title={currentStoreState.name}
+            style={{ paddingBottom: "0px" }}
+          />
+          <div className="bg-white font-light text-base pl-10 pb-2 whitespace-nowrap overflow-hidden text-ellipsis w-96 ">
+            <Icon icon="zi-location" style={{ color: "#fc8019" }} />
+            {currentStoreState.address}
+          </div>
+          {/* <StoreDetail /> */}
+          {/* <Divider /> */}
           <Suspense>
-            <Collections collections={menu.contents.collections} />
-            <Divider />
-            <ProductList categories={menu.contents.categories} />
+            {/* <Collections collections={menu.contents.collections} /> */}
+            {/* <Divider /> */}
+            <Categories categories={menu.contents.categories} />
           </Suspense>
           <Divider />
           <FloatingActionButton onClick={handleFabClick} icon={<CartIcon />} />
